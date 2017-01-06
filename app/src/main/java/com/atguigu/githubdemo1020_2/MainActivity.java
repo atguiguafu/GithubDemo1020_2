1c243b188a8120b7af1ef7d907a9fba8b2a6b3ad
atguiguafu <541433511@qq.com>
2017-01-06 11:25:54 +0800

    我是一个兵来自老百姓

--- app/src/main/java/com/atguigu/githubdemo1020_2/MainActivity.java
+++ app/src/main/java/com/atguigu/githubdemo1020_2/MainActivity.java
@@ -2,6 +2,7 @@ package com.atguigu.githubdemo1020_2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "我是一个兵，来自老百姓", Toast.LENGTH_SHORT).show();
     }
 }
